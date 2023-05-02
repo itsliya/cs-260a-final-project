7526feb4c6f90b3c7c1929791fc307a21780c2c7
Chuanyu Pan <chuanyu_pan@berkeley.edu>
2023-05-01 21:27:42 -0700

    add img and price calculation

---
+++ public/assets/js/product.js
@@ -0,0 +1,21 @@
+
+export const productImg = {
+    "bagel": "assets/img/productImg/bagel_thomas\'bagels.png",
+    "steak": "assets/img/productImg/beefsteak_OmahaSteaks.png",
+    "blackberry": "assets/img/productImg/blackberries_driscolls.png",
+    "butter": "assets/img/productImg/butter-LandofLakes.png",
+    "cake": "assets/img/productImg/cheesecake_TheCheesecakeFactory.png",
+    "chicken": "assets/img/productImg/chickenbreast-Perdue.png",
+    "chips": "assets/img/productImg/chips_lay's.png",
+    "chocolate": "assets/img/productImg/Chocolate-Hershey's.png",
+    "cream": "assets/img/productImg/cream_HorizonOrganic.png",
+    "milk": "assets/img/productImg/Oatmilk_oatly.png",
+    "peanut": "assets/img/productImg/peanutbutter-jif.png",
+    "pork": "assets/img/productImg/porkrib-smithfield.png",
+    "strawberry": "assets/img/productImg/strawberry-driscoll's.png",
+    "yogurt": "assets/img/productImg/yogurt-chobani.png",
+
+    "apple": "https://appleforthat.stemilt.com/wp-content/uploads/2016/05/Jonagold-200px.png",
+    "pear": "https://static.libertyprim.com/files/familles/poire-large.jpg?1569271830",
+    "banana": "https://domf5oio6qrcr.cloudfront.net/medialibrary/6372/202ebeef-6657-44ec-8fff-28352e1f5999.jpg"
+}
